ce743f343d7fea300dac56b642cfc58f87ab8b50
sloorjuice <antant8085@gmail.com>
2025-04-02 09:43:51 -0400

    Added underline to navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import '../css/Navbar.css';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const date_link = "https://www.google.com/search?sca_esv=b657747ebb3c27f5&sxsrf=AHTn8zq7MTJ6dSA0iuoPBp-MQ32vEXUr2g:1743553914035&q=Today%27s+date&spell=1&sa=X&ved=2ahUKEwj3kMW9jLiMAxVavokEHbX9GFkQBSgAegQICxAB";
 
@@ -25,13 +25,13 @@ function Navbar() {
                 
                 <div className="navbar-menu">
                     <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/music">Music</Link></li>
-                        <li><Link to="/pokemon">Pokemon</Link></li>
-                        <li><Link to="/games">Games</Link></li>
-                        <li><Link to="/movies">Movies</Link></li>
-                        <li><Link to="/tv">TV Shows</Link></li>
-                        <li><Link to="/books">Books</Link></li>
+                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink to="/music">Music</NavLink></li>
+                        <li><NavLink to="/pokemon">Pokemon</NavLink></li>
+                        <li><NavLink to="/games">Games</NavLink></li>
+                        <li><NavLink to="/movies">Movies</NavLink></li>
+                        <li><NavLink to="/tv">TV Shows</NavLink></li>
+                        <li><NavLink to="/books">Books</NavLink></li>
                     </ul>
                 </div>
             </div>
