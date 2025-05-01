d82a7c59f35cbc3358c0e7b264bb1fc12a0d537b
sloorjuice <antant8085@gmail.com>
2025-04-30 20:57:46 -0400

    Fixed Unused Imports

--- src/components/ClientLayout.tsx
+++ src/components/ClientLayout.tsx
@@ -10,18 +10,6 @@ import { useState, useEffect } from "react";
 import { useSwipeable } from "react-swipeable";
 import "../styles/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-  display: "swap",
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-  display: "swap",
-});
-
 export default function ClientLayout({
   children,
 }: {
